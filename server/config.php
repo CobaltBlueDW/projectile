a5c49bb510d79c6ac288191e8719380dfefc9f39
David Wipperfurth <cobaltbluedw@gmail.com>
2018-03-02 10:14:04 -0600

    Update config.php

--- server/config.php
+++ server/config.php
@@ -8,8 +8,8 @@ $config = (object)array(
         'hostname' => '127.0.0.1'
     ),
     'JIRA' => (object)array(
-        'username' => 'dwipperfurth',
-        'password' => base64_decode('dGhpcyBpcyBub3QgYSByZWFsIHBhc3N3b3Jk'),    // resist the urge to decode this ;)
+        'username' => '',
+        'password' => ''
         'domain' => 'jira.mpi.local'
     )
 );
